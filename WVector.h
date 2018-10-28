79322874733ec5143b4a634816d0b086a42573e2
C. P. Wutti <christopher.wutti@gmx.at>
2018-10-28 23:10:53 +0100

    added printing vector

--- WVector.h
+++ WVector.h
@@ -1,1 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+//vector with list of entries and the dimension
+typedef struct w_vector {
+	int *loe;
+	int dim;
+} w_vector;
+
+//printing a vector in a nice form to look at
+void wv_print(w_vector *v){
+	int i;
+	for(i=0; i < v->dim - 1; i++){
+		if(i==0) printf("( %d ) \n", v->loe[i]);
+		printf("| %d | \n", v->loe[i]);
+	}
+	printf("( %d ) \n", v->loe[i]);
+}
