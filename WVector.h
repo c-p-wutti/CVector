2adac35510e6f3945b615971d9a5cd9ed7e94ee5
C. P. Wutti <wuttic@localhost.localdomain>
2018-11-09 19:01:11 +0100

    added testing add, not working properly yet

--- WVector.h
+++ WVector.h
@@ -1,13 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//vector with list of entries and the dimension
 typedef struct w_vector {
 	int *loe;
 	int dim;
 } w_vector;
 
-//printing a vector in a nice form to look at
 void wv_print(w_vector *v){
 	int i;
 	for(i=0; i < v->dim - 1; i++){
@@ -17,7 +15,6 @@ void wv_print(w_vector *v){
 	printf("( %d ) \n", v->loe[i]);
 }
 
-//adding two vectors and returning the sum
 w_vector * wv_add(w_vector *ret, w_vector *v1, w_vector *v2){
 	int i;
 	ret->dim = v1->dim;
@@ -28,8 +25,6 @@ w_vector * wv_add(w_vector *ret, w_vector *v1, w_vector *v2){
 		int a = v1->loe[i];
 		int b = v2->loe[i];
 		ret->loe[i] = a + b;
-		//printf("first: %d, second: %d \n", v1->loe[i], v2->loe[i]);
-		
 }
 	return 	ret;
 }
--- m_test.c
+++ m_test.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "WVector.h"
+#include <time.h>
+
+int calcAddTest(w_vector v1, w_vector v2, int prop_ans[], int dim);
+void printTestResult(int test_nr, int passed, w_vector v1, w_vector v2);
+void testAdd();
 
 int main() {
 
@@ -16,9 +22,72 @@ int main() {
   v2.loe = array2;
   v2.dim = 3;
 
- wv_add(&ret, &v1, &v2);
 
+ wv_add(&ret, &v1, &v2);
  wv_print(&ret);
 
+ int sol[3] = {4,4,3};
+
+ int ret2 = calcAddTest(v1, v2, sol, 3);
+
+ testAdd();
+
+ printf("ret: %d", ret2);
+
   printf("\n\n Program finished running! \n");
+}
+
+void testAdd(){
+
+ int i;
+ int j;
+ int l;
+
+ time_t t;
+ srand((unsigned) time(&t));
+
+ w_vector v1;
+ w_vector v2;
+
+ for(i=0; i<10; i++){
+  int a1[i];
+  int a2[i];
+   for(j=0; j<i; j++){
+    a1[j]=rand() % 15000;
+    a2[j]=rand() % 15000;
+   }
+  v1.loe = a1;
+  v1.dim = i;
+  v2.loe = a2;
+  v2.dim = i;
+  int res[i];
+
+   for(l=0;l<i; i++){
+     res[l] = a1[l]+a2[l];
+   }
+
+  int suc = calcAddTest(v1,v2,res, i);
+  printTestResult(1, suc, v1,v2);
+ }
+
+}
+
+int calcAddTest(w_vector v1, w_vector v2,  int prop_ans[], int dimp){
+ int passed = 1;
+ w_vector ret;
+
+ wv_add(&ret, &v1, &v2);
+
+ int i;
+ for(i=0;i<dimp;i++){
+  if(ret.loe[i]!=prop_ans[i]) 
+    passed = 0;
+ }
+
+ return passed;
+}
+
+void printTestResult(int test_nr, int passed, w_vector v1, w_vector v2){
+ if(passed) printf("%d, passed \n", test_nr);
+ else       printf("%d, failed \n", test_nr); 
 }
